720de3a3c250d00c10d5fc56c7ba4f814a9d7a75
Shreyadb18 <shreyadb.ise@gmail.com>
2026-08-12 23:15:20 +0530

    Object oriented programming
    OOP, classes and objects, attributes, creating multiple objects

---
+++ 15_OOP.py
@@ -0,0 +1,47 @@
+#1.OOP
+#2.classes and objects
+class Car:
+    # Attributes
+    def __init__(self, brand, model):
+        self.brand = brand  # Instance variable
+        self.model = model  # Instance variable
+
+    # Method
+    def display_info(self):
+        print(f"Car Brand: {self.brand}, Model: {self.model}")
+
+# Creating an object of the class
+my_car = Car("Toyota", "Corolla")
+my_car.display_info()
+
+#3.Attributes and methods
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        print(f"Hello, my name is {self.name} and I am {self.age} years old.")
+
+# Creating objects
+person1 = Person("Arjun", 30)
+person2 = Person("Megha", 25)
+
+person1.greet()
+person2.greet()
+
+#4.creating multiple objects from a class
+class Dog:
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        print(f"{self.name} is barking!")
+
+# Creating multiple objects
+dog1 = Dog("Rex", "Golden Retriever")
+dog2 = Dog("Bolt", "Beagle")
+
+dog1.bark()
+dog2.bark()
